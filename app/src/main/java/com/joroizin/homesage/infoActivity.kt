ddcd036edc0f5c401ae82e0a3527f0eeaecabf75
jon <joroizin@gmail.com>
2020-05-19 21:49:12 +0300

    real first commit

---
+++ app/src/main/java/com/joroizin/homesage/infoActivity.kt
@@ -0,0 +1,12 @@
+package com.joroizin.homesafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class infoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
